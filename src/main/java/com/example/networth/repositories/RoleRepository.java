4332487e56a7cdc20f6e80d54009fb02e23f336e
Anthony Stevens <aastevens1126@gmail.com>
2022-12-02 15:01:19 -0600

    User and Role models and repositories created

---
+++ src/main/java/com/example/networth/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.networth.repositories;
+
+import com.example.networth.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
