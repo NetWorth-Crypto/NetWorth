9a008f37a2bb94a4bf6b7dea356ab992738dcec5
midoostar1 <95588511+midoostar1@users.noreply.github.com>
2022-12-02 16:23:48 -0600

    working on user controller, login, logout

--- src/main/java/com/example/networth/controllers/UserController.java
+++ src/main/java/com/example/networth/controllers/UserController.java
@@ -1,12 +1,48 @@
 package com.example.networth.controllers;
 
+import com.example.networth.models.User;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class UserController {
-    @GetMapping("/login")
-    public String index(){
-        return "landing";
+
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        System.out.println("reached");
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        System.out.println("reached");
+
+        return "redirect:/login";
     }
 
 
+    @GetMapping("/login")
+    public String showLoginForm(Model model){
+        model.addAttribute("user", new User());
+        System.out.println("reached");
+        return "users/login";
+    }
+
+    @PostMapping("/login")
+    public String LoginUser(@ModelAttribute User user){
+        System.out.println("reached");
+
+        return "redirect:/landing";
+    }
+
+
+
 }
+
+
+
+
+
