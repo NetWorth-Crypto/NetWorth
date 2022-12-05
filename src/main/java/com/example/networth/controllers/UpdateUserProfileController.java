7a666460ec5dfb7fd78d6f1175a87590a248ff74
Casanova Geary <CasanovaGeary@Gmail.com>
2022-12-05 16:46:13 -0600

    Initial Feed page.

--- src/main/java/com/example/networth/controllers/UpdateUserProfileController.java
+++ src/main/java/com/example/networth/controllers/UpdateUserProfileController.java
@@ -1,6 +1,12 @@
 package com.example.networth.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class UpdateUserProfileController
 {
-
+    @GetMapping("/update-profile")
+    public String update()
+    {
+        return "update-profile";
+    }
 }
