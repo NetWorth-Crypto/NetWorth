b78fff3e0bed65e66d1e15f77f67a00411d2eb4a
Casanova Geary <CasanovaGeary@Gmail.com>
2022-12-06 11:42:15 -0600

    Basic profile with bootstrap added so we can work on fucntionality, then design later.

--- src/main/java/com/example/networth/controllers/UpdateUserProfileController.java
+++ src/main/java/com/example/networth/controllers/UpdateUserProfileController.java
@@ -1,7 +1,8 @@
 package com.example.networth.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class UpdateUserProfileController
 {
     @GetMapping("/update-profile")
