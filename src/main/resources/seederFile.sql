4726d8632d489f222d89b28a49096eb5fd438947
Anthony Stevens <aastevens1126@gmail.com>
2022-12-06 14:15:02 -0600

    Portfolio and Asset model update; Seedfile complete

--- src/main/resources/seederFile.sql
+++ src/main/resources/seederFile.sql
@@ -14,8 +14,22 @@ insert into portfolio(name,dollar_limit,is_default,is_private,user_id)
 values ('portfolio1',1000, true, false, 1),
        ('portfolio2',1000,true, false,2);
 
-insert into asset(name,ticker,current_price,purchase_price,quantity,portfolio_id)
-values ('Bitcoin','BTC',19000,23000, 1.2, )
+insert into asset(name,ticker,current_price,purchase_price)
+values ('Bitcoin','BTC',19000,23000),
+       ('Ethereum','ETH',1200, 1800);
+
+insert into portfolio_asset(portfolio_id, asset_id, quantity)
+values (1,1,1.2),(1,2,2.2),(2,1,.8),(2,2,4);
+
+insert into post(title,description,user_id)
+values ('Anthony\'s first post','Creating the next big thing', 1),
+       ('Cas\'s first post', 'Jp makes the world go around',2);
+
+insert into comment(comment,posting_user_id, post_id)
+values ('I 100% agree!',1,2),
+       ('Good luck!',2,1);
+
+
 
 
 
